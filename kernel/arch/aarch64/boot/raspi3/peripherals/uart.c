ab7f74a807d108cdffbf6c5ec36c51b105fbd05c
Franktjp <1115303890@qq.com>
2024-01-07 12:32:22 +0800

    Lab1: System boot and kernel page table init.

--- kernel/arch/aarch64/boot/raspi3/peripherals/uart.c
+++ kernel/arch/aarch64/boot/raspi3/peripherals/uart.c
@@ -141,6 +141,9 @@ void uart_send_string(char *str)
 {
         /* LAB 1 TODO 3 BEGIN */
         /* BLANK BEGIN */
+        for (unsigned int i = 0; str[i] != 0; ++i) {
+			early_uart_send((unsigned int)str[i]);
+		}
         /* BLANK END */
         /* LAB 1 TODO 3 END */
 }
